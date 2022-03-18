478889a5dfb77b53ae5b0d409fe6cad05efe3ac8
shivprajapat <shivkumarprajapat232@gmail.com>
2022-03-18 17:02:45 +0530

    Product Details Page Added

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import Rating from './Rating';
 
-const Product = (props) => {
-    const { product } = props;
-    return (
-    <div className="card">
-    <a href={`product/${product._id}`}>
-      <img className="medium" src={product.image} alt={product.name} />
-    </a>
-    <div className="card-body">
-    <a href={`product/${product._id}`}>
-        <h2>{product.name}</h2>
+export default function Product(props) {
+  const { product } = props;
+  return (
+    <div key={product._id} className="card">
+      <a href={`/product/${product._id}`}>
+        <img className="medium" src={product.image} alt={product.name} />
       </a>
-        <Rating rating={product.rating} numReviews={product.numReviews} />
-      <div className="price">{product.price}</div>
+      <div className="card-body">
+        <a href={`/product/${product._id}`}>
+          <h2>{product.name}</h2>
+        </a>
+        <Rating
+          rating={product.rating}
+          numReviews={product.numReviews}
+        ></Rating>
+        <div className="price">${product.price}</div>
+      </div>
     </div>
-  </div>
-  )
-};
-
-export default Product;
+  );
+}
